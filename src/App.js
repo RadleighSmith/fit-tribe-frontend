19b2c1b29b62bb76acb3950c3af128464ff4011a
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-13 10:05:09 +0000

    Add initial dependencies

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to FitTribe
+        </p>
+        <p>
+          The Fitness Social Media App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
