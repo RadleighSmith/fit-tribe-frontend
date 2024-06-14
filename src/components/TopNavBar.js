b6d007cc2cc7bbfa41480daf5e41a87cc85a13ad
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-14 11:27:55 +0000

    Add additional styling to Top Nav Bar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -2,18 +2,40 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../assets/fittribe_logo_coloured.png';
 import styles from '../styles/TopNavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const TopNavBar = () => {
     return (
         <Navbar className={styles.TopNavBar} expand="md" fixed='top'>
             <Container>
-                <Navbar.Brand><img src={logo} alt='FitTribe Logo' height='45' /></Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand><img src={logo} alt='FitTribe Logo' height='45' /></Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link className={styles.navlink}>Home</Nav.Link>
-                        <Nav.Link className={styles.navlink}>Login</Nav.Link>
-                        <Nav.Link className={styles.navbutton}>Sign Up</Nav.Link>
+                        <NavLink
+                            exact
+                            to='/'
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            to='/signin'
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            Login
+                        </NavLink>
+                        <NavLink
+                            to='/signup'
+                            className={styles.NavButton}
+                            activeClassName={styles.NavButtonActive}
+                        >
+                            Sign Up
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
