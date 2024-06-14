76b23d9582512e99232ac5f57cab942b68fafbbb
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-14 19:45:55 +0000

    Add signUpForm, styling and app.js route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import styles from "./App.module.css";
 import TopNavBar from "./components/TopNavBar";
 import Container from "react-bootstrap/Container"
 import {Route, Switch} from "react-router-dom"
+import './api/axiosDefaults'
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
     <div className={styles.App}>
       <TopNavBar />
-      <Container className={styles.Main}>
+      <Container fluid className={styles.Main}>
         <Switch>
           <Route exact path ="/"  render={() => <h1>Home Page</h1>} />
           <Route exact path ="/signin"  render={() => <h1>Login</h1>} />
-          <Route exact path ="/signup"  render={() => <h1>Sign Up</h1>} />
+          <Route exact path ="/signup"  render={() => <SignUpForm />} />
           <Route render={() => <h1 className="text-center">Uh Oh! Page Not Found!</h1>}/>
         </Switch>
       </Container>
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import logo from "../../assets/fittribe_logo_coloured.png";
+import signUpImage from "../../assets/sign_up_image.jpg";
+import styles from "../../styles/LoginSignUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+      username: "",
+      email: "",
+      password1: "",
+      password2: "",
+    });
+    const { username, email, password1, password2 } = signUpData;
+  
+    const [errors, setErrors] = useState({});
+  
+    const history = useHistory();
+  
+    const handleChange = (event) => {
+      setSignUpData({
+        ...signUpData,
+        [event.target.name]: event.target.value,
+      });
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        await axios.post("/dj-rest-auth/registration/", signUpData);
+        history.push("/signin");
+      } catch (err) {
+        setErrors(err.response?.data);
+      }
+    };
+  
+    return (
+        <Row className={'no-gutters'}>
+            <Col className={styles.LeftCol} md={6}>
+                <div className={styles.Circle} />
+                <div className={styles.InnerCircle} />
+                <Image className={styles.FormImage} src={signUpImage} />
+            </Col>
+            <Col className={`my-auto p-sm-2 p-md-3 p-lg-5 ${styles.RightCol}`} md={6}>
+                <Container className={`${appStyles.Content} p-4`}>
+                    <img src={logo} alt='FitTribe Logo' className={styles.Logo} height='60' />
+                    <h1 className={styles.FormTitle}>Sign Up</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="text"
+                                placeholder="Enter Username"
+                                name="username"
+                                value={username}
+                                onChange={handleChange}
+                            />
+                            {errors.username?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            ))}
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.Label className="d-none">Email</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="email"
+                                placeholder="Enter Email"
+                                name="email"
+                                value={email}
+                                onChange={handleChange}
+                            />
+                            {errors.email?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            ))}
+                        </Form.Group>
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Enter Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="password"
+                                placeholder="Enter Password"
+                                name="password1"
+                                value={password1}
+                                onChange={handleChange}
+                            />
+                            {errors.password1?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            ))}
+                        </Form.Group>
+                        <Form.Group controlId="password2">
+                            <Form.Label className="d-none">Confirm Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="password"
+                                placeholder="Confirm Password"
+                                name="password2"
+                                value={password2}
+                                onChange={handleChange}
+                            />
+                            {errors.password2?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>{message}</Alert>
+                            ))}
+                        </Form.Group>
+                        <Link className={styles.Link} to="/signin">
+                            Already have a FitTribe account? <span>Sign in here</span>
+                        </Link>
+                        <Button
+                            className={btnStyles.Button}
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
+                    </Form>
+                </Container>
+            </Col>
+        </Row>
+    );
+};
+
+export default SignUpForm;
+
