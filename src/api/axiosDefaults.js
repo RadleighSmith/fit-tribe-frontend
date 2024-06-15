0bebcb13972aec44a8859244ffda62113363eb84
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-15 13:58:00 +0000

    Implement JWT Refresh Token Handling

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://fit-tribe-api-97fb1c20a2ee.herokuapp.com';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
