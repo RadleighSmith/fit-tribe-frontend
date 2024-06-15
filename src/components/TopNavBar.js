2a0829a713792994fd433109a6153ce6682c64d3
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-15 09:23:58 +0000

    Update TopNavBar with conditional rendering based on user authentication state

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -3,8 +3,38 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../assets/fittribe_logo_coloured.png';
 import styles from '../styles/TopNavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 const TopNavBar = () => {
+
+    const currentUser = useCurrentUser();
+    const loggedInLinks = <>{currentUser?.username}</>
+    const loggedOutLinks = (
+        <>
+            <NavLink
+                exact
+                to='/'
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+            >
+                Home
+            </NavLink>
+            <NavLink
+                to='/signin'
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+            >
+                Login
+            </NavLink>
+            <NavLink
+                to='/signup'
+                className={styles.NavButton}
+                activeClassName={styles.NavButtonActive}
+            >
+                Sign Up
+            </NavLink>
+        </>
+    )
     return (
         <Navbar className={styles.TopNavBar} expand="md" fixed='top'>
             <Container fluid >
@@ -14,28 +44,7 @@ const TopNavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <NavLink
-                            exact
-                            to='/'
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                        >
-                            Home
-                        </NavLink>
-                        <NavLink
-                            to='/signin'
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                        >
-                            Login
-                        </NavLink>
-                        <NavLink
-                            to='/signup'
-                            className={styles.NavButton}
-                            activeClassName={styles.NavButtonActive}
-                        >
-                            Sign Up
-                        </NavLink>
+                    {currentUser ? loggedInLinks : loggedOutLinks}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
