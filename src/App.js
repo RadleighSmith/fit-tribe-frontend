49c51613bab0bf6bde061cd02f49ffc5d644d6cf
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-16 17:08:54 +0000

    Add backgound image, adjusted colour of scrollbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
+import background from "./styles/Background.module.css"
 import TopNavBar from "./components/TopNavBar";
-import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
@@ -13,17 +13,17 @@ function App() {
   const currentUser = useCurrentUser();
 
   return (
-    <div className={styles.App}>
+    <div className={`${styles.App} ${background.Background}`}>
       <TopNavBar />
       {currentUser && <SideNavBar />}
-      <Container fluid className={currentUser ? styles.MainWithSidebar : styles.Main}>
+      <div className={currentUser ? styles.MainWithSidebar : styles.Main}>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/signin" component={LoginForm} />
           <Route exact path="/signup" component={SignUpForm} />
           <Route render={() => <h1 className="text-center">Uh Oh! Page Not Found!</h1>} />
         </Switch>
-      </Container>
+      </div>
     </div>
   );
 }
