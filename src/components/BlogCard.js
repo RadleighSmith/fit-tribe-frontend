295b5e483c708c9418088a2f0dc31875f4ae3772
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-17 13:33:39 +0000

    Add Blogs List page, Blog card component and styling

---
+++ src/components/BlogCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Image } from 'react-bootstrap';
+import appStyles from '../App.module.css'
+import styles from '../styles/BlogCard.module.css';
+
+const BlogCard = ({ blog }) => {
+    const defaultBlogImage = 'https://res.cloudinary.com/dn6vitvd4/image/upload/v1/fittribe_media/../default_post_eznpr6';
+
+    return (
+        <Card className={appStyles.Content}>
+            <Card.Body>
+                <div className={styles.BlogHeader}>
+                    <Image src={blog.profile_image} roundedCircle className={styles.ProfileImage} />
+                    <div className={styles.BlogInfo}>
+                        <Card.Title className={styles.Title}>{blog.title}</Card.Title>
+                        <div className={styles.OwnerAndDate}>
+                            <span className={styles.OwnerName}>{blog.owner}</span>
+                            <span className={styles.DatePosted}>{blog.created_at}</span>
+                        </div>
+                    </div>
+                </div>
+                <Card.Text>{blog.content.slice(0, 400)}... <Link to={`/blogs/${blog.id}`}>Read More</Link></Card.Text>
+                {blog.image && blog.image !== defaultBlogImage && (
+                    <Card.Img variant="bottom" src={blog.image} className={styles.BlogImage} />
+                )}
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default BlogCard;
---
+++ src/pages/blogs/BlogsPage.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Button} from 'react-bootstrap';
+import axios from 'axios';
+import appStyles from '../../App.module.css';
+import divider from '../../styles/Divider.module.css'
+import styles from '../../styles/BlogsPage.module.css';
+import BlogCard from '../../components/BlogCard';
+import btnStyles from '../../styles/Button.module.css';
+
+const BlogsPage = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [errors, setErrors] = useState(null);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const response = await axios.get('/blogs/');
+                setBlogs(Array.isArray(response.data.results) ? response.data.results : response.data);
+            } catch (err) {
+                setErrors(err.response?.data);
+            }
+        };
+
+        fetchBlogs();
+    }, []);
+
+    return (
+        <Container>
+            <Row className={appStyles.Content}>
+                <Col className="text-center p-3">
+                    <h1 className={styles.Title}>Blogs</h1>
+                    {errors && <p className="text-danger">{errors.detail}</p>}
+                    <hr className={divider.BlueDivider} />
+                    <Link to="/create-blog">
+                        <Button className={`${btnStyles.Button} ${btnStyles.ButtonWide} mt-4`}>Create a New Blog</Button>
+                    </Link>
+                </Col>
+            </Row>
+            <Row>
+                {Array.isArray(blogs) && blogs.map((blog) => (
+                    <Col key={blog.id} xs={12} className="mt-4">
+                        <BlogCard blog={blog} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default BlogsPage;
+
