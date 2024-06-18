895e39a48d614b98ee6e1e774dc2d4e34da3cd6a
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-18 15:39:37 +0000

    BUG: Fixed bug with side scrolling on user comments

--- src/components/BlogComment.js
+++ src/components/BlogComment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Media, Image } from 'react-bootstrap';
+import styles from '../styles/Comment.module.css';
 import profileStyles from '../styles/ProfilePicture.module.css';
 
 const BlogComment = ({ owner, profile_id, profile_image, comment, created_at }) => {
@@ -13,7 +14,7 @@ const BlogComment = ({ owner, profile_id, profile_image, comment, created_at }) 
                 <Link to={`/profiles/${profile_id}`} className={profileStyles.ProfileLink}>
                     <h5 className="mt-0">{owner}</h5>
                 </Link>
-                <p>{comment}</p>
+                <p className={styles.CommentText}>{comment}</p>
                 <small className="text-muted">{created_at}</small>
             </Media.Body>
         </Media>
