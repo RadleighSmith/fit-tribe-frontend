8590c16c5e4b573a4ae3a4c67de23eac1fe2e868
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-20 17:34:11 +0000

    Fix: Removed Unused CSS import

--- src/components/blogs/BlogEditComment.js
+++ src/components/blogs/BlogEditComment.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { axiosRes } from '../../api/axiosDefaults';
-import styles from '../../styles/Comment.module.css';
 import btnStyles from '../../styles/Button.module.css';
 
 const BlogEditComment = ({ id, comment, setComments, setShowEditForm }) => {
