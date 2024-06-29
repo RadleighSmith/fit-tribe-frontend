8d2f8a55a1b1ff964bbee9419ec9b4e1ca619801
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-29 11:14:36 +0000

    Feat: Updated styling on groups lists

--- src/pages/groups/GroupsPage.js
+++ src/pages/groups/GroupsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Row, Col, Image, Button, Spinner, Alert } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
+import { Container, Row, Col, Image, Button, Spinner, Alert, Card } from 'react-bootstrap';
 import axios from 'axios';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import appStyles from '../../App.module.css';
@@ -15,6 +15,7 @@ const GroupsPage = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const currentUser = useCurrentUser();
+  const history = useHistory();
 
   useEffect(() => {
     const fetchGroups = async () => {
@@ -57,6 +58,10 @@ const GroupsPage = () => {
     }
   };
 
+  const handleCardClick = (groupId) => {
+    history.push(`/groups/${groupId}`);
+  };
+
   if (loading) return <Spinner animation="border" />;
   if (error) return <Alert variant="danger">Failed to load groups</Alert>;
 
@@ -68,11 +73,9 @@ const GroupsPage = () => {
           <hr className={divider.BlueDivider} />
           {error && <p className="text-danger">Failed to load groups</p>}
           {currentUser && (currentUser.is_superuser || currentUser.is_staff) && (
-            <>
-              <Link to="/create-group">
-                <Button className={`${btnStyles.Button} ${btnStyles.ButtonWide} mt-2`}>Create a New Group</Button>
-              </Link>
-            </>
+            <Link to="/create-group">
+              <Button className={`${btnStyles.Button} ${btnStyles.ButtonWide} mt-2`}>Create a New Group</Button>
+            </Link>
           )}
         </Col>
       </Row>
@@ -80,43 +83,46 @@ const GroupsPage = () => {
         <Col>
           {groups.length ? (
             groups.map((group) => (
-              <div key={group.id}>
-                <Row className='my-3 align-items-center'>
-                  <Col xs={12} sm={2} className="d-flex justify-content-center">
-                    <Image src={group.group_logo} thumbnail className={groupStyles.GroupLogo} />
-                  </Col>
-                  <Col xs={12} sm={6}>
-                    <Link to={`/groups/${group.id}`} className={groupStyles.GroupLink}>
-                      <h4>{group.name}</h4>
-                    </Link>
-                    <p>{group.description}</p>
-                  </Col>
-                  <Col xs={12} sm={4} className="d-flex justify-content-center justify-content-sm-end mt-2 mt-sm-0">
-                    {currentUser && (
-                      <>
-                        {group.is_member ? (
-                          <Button
-                            variant="danger"
-                            onClick={() => handleLeaveGroup(group.id)}
-                            className={`${btnStyles.ButtonRed} ${btnStyles.ButtonLarge} mx-2`}
-                          >
-                            Leave Group
-                          </Button>
-                        ) : (
-                          <Button
-                            variant="primary"
-                            onClick={() => handleJoinGroup(group.id)}
-                            className={`${btnStyles.Button} ${btnStyles.ButtonLarge} mx-2`}
-                          >
-                            Join Group
-                          </Button>
-                        )}
-                      </>
-                    )}
-                  </Col>
-                </Row>
-                <hr className={divider.OffWhiteDivider} />
-              </div>
+              <Card
+                key={group.id}
+                className={`${groupStyles.GroupCard} my-3`}
+                onClick={() => handleCardClick(group.id)}
+              >
+                <Card.Body>
+                  <Row className="align-items-center">
+                    <Col xs={12} sm={2} className="d-flex justify-content-center">
+                      <Image src={group.group_logo} thumbnail className={groupStyles.GroupLogo} />
+                    </Col>
+                    <Col xs={12} sm={6}>
+                      <h4 className={groupStyles.GroupLink}>{group.name}</h4>
+                      <p>{group.description}</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="d-flex justify-content-center justify-content-sm-end mt-2 mt-sm-0">
+                      {currentUser && (
+                        <>
+                          {group.is_member ? (
+                            <Button
+                              variant="danger"
+                              onClick={(e) => { e.stopPropagation(); handleLeaveGroup(group.id); }}
+                              className={`${btnStyles.ButtonRed} ${btnStyles.ButtonLarge} mx-2`}
+                            >
+                              Leave Group
+                            </Button>
+                          ) : (
+                            <Button
+                              variant="primary"
+                              onClick={(e) => { e.stopPropagation(); handleJoinGroup(group.id); }}
+                              className={`${btnStyles.Button} ${btnStyles.ButtonLarge} mx-2`}
+                            >
+                              Join Group
+                            </Button>
+                          )}
+                        </>
+                      )}
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
             ))
           ) : (
             <p>No groups available.</p>
